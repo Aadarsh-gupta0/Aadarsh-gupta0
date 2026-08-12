0f62ed59957a6983330b6bafbee84f84001b7e16
Aadarsh Gupta <aadarshgupta.ag2006@gmail.com>
2026-08-12 20:08:54 +0530

    Delete JobPulse directory

--- JobPulse/backend/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""HTTP API routers."""
-
-from .admin import router as admin_router
-from .devices import router as devices_router
-from .jobs import router as jobs_router
-
-__all__ = ["admin_router", "devices_router", "jobs_router"]
--- JobPulse/backend/app/push/__init__.py
+++
@@ -1,6 +0,0 @@
-"""APNs delivery."""
-
-from .apns import APNsClient, PushResult
-from .payload import build_payload, collapse_id_for, thread_id_for
-
-__all__ = ["APNsClient", "PushResult", "build_payload", "collapse_id_for", "thread_id_for"]
